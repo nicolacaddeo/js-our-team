a279adae405f745de5791fc03400aa32f2c3993a
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-06 18:11:11 +0200

    created team objects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 /*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-
-Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
 
@@ -12,3 +8,37 @@ Stampare su console le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 */
+
+// oggetti - team
+const memberWayne = {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg'
+};
+const memberAngela = {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg'
+};
+const memberWalter = {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg'
+};
+const memberAngelaLopez = {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg'
+};
+const memberScott = {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg'
+};
+const memberBarbara = {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg'
+};
+
+
